5b154e94d4c080294f58fa5e4fa1b6537b25c6b9
kabirSohail <kabeersohel@gmail.com>
2018-05-05 20:06:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sohail/macha/InfiniteCycleViewPager.java
@@ -0,0 +1,72 @@
+package com.example.sohail.macha;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.firebase.ui.auth.AuthUI;
+import com.gigamole.infinitecycleviewpager.HorizontalInfiniteCycleViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InfiniteCycleViewPager extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
+
+    List<Integer> lstImages = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infinite_cycle_view_pager);
+
+        drawerLayout = findViewById(R.id.drawermaccha);
+        navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        initData();
+
+        HorizontalInfiniteCycleViewPager pager = (HorizontalInfiniteCycleViewPager)findViewById(R.id.horizontal_cycle);
+        MyAdapter adapter = new MyAdapter(lstImages,getBaseContext());
+        pager.setAdapter(adapter);
+
+    }
+
+    private void initData() {
+        lstImages.add(R.drawable.fbg1);
+        lstImages.add(R.drawable.fbg2);
+        lstImages.add(R.drawable.fbg3);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.signoutdrawar){
+                AuthUI.getInstance().signOut(this);
+                startActivity(new Intent(this,MainActivity.class));
+            }
+
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
